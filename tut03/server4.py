4a4d113bdef4f7fc7291cc8a3b9acae2a5b5b694
Angelina612 <75089276+Angelina612@users.noreply.github.com>
2023-03-28 21:41:38 +0530

    server 4 updated
    Echo server
    Single process server that uses the
    “select” method to handle multiple clients concurrently

---
+++ tut03/server4.py
@@ -0,0 +1,72 @@
+from socket import *
+import sys
+from select import *
+import queue
+
+serverName = sys.argv[1]
+serverPort = int(sys.argv[2])
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((serverName,serverPort))
+serverSocket.listen(1)
+print ('The server is ready to receive')
+
+inputs = [serverSocket]
+outputs = []
+
+message_queues = {}
+
+while inputs:
+    readable, writable, exceptional = select(inputs, outputs, inputs)
+
+    # Handle inputs
+    for s in readable:
+        if s is serverSocket:
+            # A "readable" server socket is ready to accept a connection
+            connectionSocket, addr = s.accept()
+            print('Connected with client ', addr, sep='')
+            
+            connectionSocket.setblocking(0)
+            inputs.append(connectionSocket)
+
+            # Give the connection a queue for data we want to send
+            message_queues[connectionSocket] = queue.Queue()
+        else:
+            data = s.recv(1024).decode()
+
+            if not data:
+                # Stop listening for input on the connection
+                if s in outputs:
+                    outputs.remove(s)
+                inputs.remove(s)
+                s.close()
+                # Remove message queue
+                del message_queues[s]
+
+            else:
+                # A readable client socket has data
+                print('Client ',addr, ' sent message: ',data, sep='')
+
+                message_queues[s].put(data)
+                # Add output channel for response
+                if s not in outputs:
+                    outputs.append(s)
+
+    # Handle outputs
+    for s in writable:
+        try:
+            message = message_queues[s].get_nowait()
+        except queue.Empty:
+            # No messages waiting so stop checking for writability.
+            outputs.remove(s)
+        else:
+            print('Sending reply: ', message, sep='')
+            s.send(str(message).encode())
+
+    # Handle "exceptional conditions"
+    for s in exceptional:
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        s.close()
+        # Remove message queue
+        del message_queues[s]
