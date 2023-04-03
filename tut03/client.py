c8bfa0b5dc20e78953dc543f743b4ff1a2d74dac
Angelina612 <75089276+Angelina612@users.noreply.github.com>
2023-04-03 23:42:22 +0530

    Update client.py

--- tut03/client.py
+++ tut03/client.py
@@ -8,12 +8,19 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 print('Connected to server')
 
-while True:
+f = True
+while f:
     expression = input('Please enter the message to the server: ')
     clientSocket.send(expression.encode())
     answer = clientSocket.recv(1024).decode()
     print ('Server replied: ',  answer, sep='')
     y = input('Do you wish to continue (Y/N)? ')
-    if y != 'Y' and y != 'y':
-        clientSocket.close()
-        break
+    
+    while True:
+        if y == 'N' or y == 'n':
+            clientSocket.close()
+            f = False
+            break
+        elif y == 'y' or y == 'Y':
+            break
+        y = input('Please enter Y or N: ')
